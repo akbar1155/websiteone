a2d183e1079c6f632128bc73c30834168bdc49df
Akbar <isroilovakbar49@gmail.com>
2023-03-31 21:06:36 +0500

    904580

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import { Nav } from "../src/components/Navbar/nav";
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      {/* <Nav  /> */}
+      <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut laudantium voluptatem animi tenetur sint quae at aliquid rerum odit, exercitationem repellendus veritatis obcaecati quis reprehenderit vel quasi omnis nobis. Magnam!</h2>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Navbar/nav.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Akbarlogo } from "../image/akbarlogo.jpg";
+
+const Nav = () => {
+    return (
+        <div>
+            <h1>Lorem ipsum dolor sit, amet consectetur adipisicing elit. At incidunt aliquid dolorum dolore iure aperiam, ex nihil. Facere odit libero placeat atque itaque, a explicabo mollitia temporibus, consectetur, nesciunt suscipit!</h1>
+            <img src="Akbarlogo" alt="" />
+        </div>
+    );
+}
+
+export default Nav;
